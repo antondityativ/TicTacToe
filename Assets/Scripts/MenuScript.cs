5a6171a3b21f1aa834953eb5ec29e54d85a1b528
Anton Dityativ <anton_dityativ@mail.ru>
2015-02-16 17:21:23 +0600

    alignment

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
-		public int PlayerIndex = 0;
+	public int PlayerIndex = 0;
 
-		public void NewGameButton() {
-				Application.LoadLevel ("gameScene");
-				PlayerIndex = 1;
-		}
+	public void NewGameButton() {
+		Application.LoadLevel ("gameScene");
+		PlayerIndex = 1;
+	}
 
-		public void VSComputer(){
-				Application.LoadLevel ("gameScene");
-				PlayerIndex = 2;
-		}
+	public void VSComputer(){
+		Application.LoadLevel ("gameScene");
+		PlayerIndex = 2;
+	}
 
-		public void QuitButton() {
-				Application.Quit ();
-		}
+	public void QuitButton() {
+		Application.Quit ();
+	}
 }
